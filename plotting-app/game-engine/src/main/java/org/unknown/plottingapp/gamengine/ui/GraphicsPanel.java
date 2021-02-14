6fc68ac4e6823e27d0c7c2c6cbf0a559c7757e53
yashrahmed <yashrahmed@gmail.com>
2021-02-14 15:06:27 -0600

    Drawing grids with configurable spacing

--- plotting-app/game-engine/src/main/java/org/unknown/plottingapp/gamengine/ui/GraphicsPanel.java
+++ plotting-app/game-engine/src/main/java/org/unknown/plottingapp/gamengine/ui/GraphicsPanel.java
@@ -20,6 +20,7 @@ import java.net.URL;
 
 public class GraphicsPanel extends JPanel implements ActionListener {
 
+    private static final int GRID_SPACING = 10;
     private final GameState gameState;
     private final CommandAdapter adapter;
     private final boolean isHistoryPlotEnabled;
@@ -46,31 +47,53 @@ public class GraphicsPanel extends JPanel implements ActionListener {
         Toolkit.getDefaultToolkit().sync();
     }
 
-    private void drawPanel(Graphics graphics) {
-        Graphics2D g2d = (Graphics2D) graphics;
+    private void drawCourseHistory(Graphics2D graphics2D) {
+        graphics2D.setColor(Color.orange);
+        this.gameState.getPositionHistory().forEach(
+                position -> graphics2D.drawOval(
+                        Math.round(position.x) + (this.shipImage.getWidth(this) / 2),
+                        Math.round(position.y) + (this.shipImage.getHeight(this) / 2),
+                        5, 5));
+    }
+
+    private void drawBoat(Graphics2D graphics2D) {
         int x_center = Math.round(this.gameState.getCurrentPosition().x)
                 + (this.shipImage.getWidth(this) / 2);
         int y_center = Math.round(this.gameState.getCurrentPosition().y)
                 + (this.shipImage.getHeight(this) / 2);
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2d.rotate(this.gameState.getDirection(), x_center, y_center);
-        g2d.drawImage(this.shipImage, Math.round(this.gameState.getCurrentPosition().x),
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics2D.rotate(this.gameState.getDirection(), x_center, y_center);
+        graphics2D.drawImage(this.shipImage, Math.round(this.gameState.getCurrentPosition().x),
                 Math.round(this.gameState.getCurrentPosition().y), this);
-        g2d.rotate(-this.gameState.getDirection(), x_center, y_center);
-        g2d.setColor(Color.orange);
+        graphics2D.rotate(-this.gameState.getDirection(), x_center, y_center);
+    }
+
+    private void drawGrid(Graphics2D graphics2D) {
+        // draw vertical
+        for (int i = GRID_SPACING; i <  this.getWidth(); i+= GRID_SPACING) {
+            graphics2D.drawLine(i, 0, i, this.getHeight());
+        }
+        // draw horizontal
+        for (int i = GRID_SPACING; i <  this.getWidth(); i+= GRID_SPACING) {
+            graphics2D.drawLine(0, i, this.getWidth(), i);
+        }
+    }
+
+    private void drawPanel(Graphics graphics) {
+        Graphics2D g2d = (Graphics2D) graphics;
+        drawBoat(g2d);
+        drawGrid(g2d);
         if (this.isHistoryPlotEnabled) {
-            this.gameState.getPositionHistory().forEach(
-                    position -> g2d.drawOval(
-                            Math.round(position.x) + (this.shipImage.getWidth(this) / 2),
-                            Math.round(position.y) + (this.shipImage.getHeight(this) / 2),
-                            5, 5));
+            drawCourseHistory(g2d);
         }
         g2d.dispose();
     }
 
     private void initPanel(int frameWidth, int frameHeight, Timer timer) {
+        Dimension frameDimension = new Dimension(frameWidth, frameHeight);
         addKeyListener(this.adapter);
-        setMinimumSize(new Dimension(frameWidth, frameHeight));
+        setMinimumSize(frameDimension);
+        setSize(frameDimension);
         setLayout(null);
         setBackground(Color.black);
         setFocusable(true);
